233a779dbe64ca59dc916c28cc9fc2167dac1840
MarMaksk <87898650+MarMaksk@users.noreply.github.com>
2021-08-18 15:21:22 +0300

    Add files via upload

---
+++ company/Thread/ThreadOutputInputProductList.java
@@ -0,0 +1,51 @@
+package com.company.Thread;
+
+import com.company.Production.Product;
+import com.company.Warehouse;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class ThreadOutputInputProductList implements Runnable {
+    private volatile Warehouse warehouse;
+    private volatile boolean output = true;
+    private volatile File file;
+
+
+    public ThreadOutputInputProductList(Warehouse warehouse, boolean output) {
+        this.warehouse = warehouse;
+        this.output = output;
+        this.file = new File("warehouse " + warehouse.getAdress() + " product list" + ".dat");
+    }
+
+    @Override
+    public void run() {
+        if (output == true) {
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+                file.createNewFile();
+                oos.writeObject(warehouse.getProductsList());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                ArrayList<Product> pt = (ArrayList<Product>) ois.readObject();
+                pt.forEach(System.out::println);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ company/Thread/ThreadOutputInputWaybillList.java
@@ -0,0 +1,50 @@
+package com.company.Thread;
+
+import com.company.Warehouse;
+import com.company.Waybill;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class ThreadOutputInputWaybillList implements Runnable {
+    private Warehouse warehouse;
+    private boolean output = true;
+
+    public ThreadOutputInputWaybillList(Warehouse warehouse, boolean output) {
+        this.warehouse = warehouse;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        if (output == true) {
+            File file2 = new File("warehouse" + warehouse.getAdress() + " waybill list" + ".dat");
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file2))) {
+                file2.createNewFile();
+                oos.writeObject(warehouse.getWaybillList());
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            File file2 = new File("warehouse" + warehouse.getAdress() + " waybill list" + ".dat");
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file2))) {
+                LinkedList<Waybill> pt1 = (LinkedList<Waybill>) ois.readObject();
+                pt1.forEach(System.out::println);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
